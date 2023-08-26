eacde0056568ca62933cebe85e1b130927c6a683
Sivaka <143190785+Sivaka86@users.noreply.github.com>
2023-08-26 03:33:48 -0700

    Create add_values.sql

---
+++ add_values.sql
@@ -0,0 +1,6 @@
+-- Adding values into the table 
+insert into Customers values (1,'John', 'Ray','123 Avenue',283883,'ass123@gmail.com')
+select * from Customers
+
+--  adding  values only not null fileds 
+insert into Customers values (2,'Niro', 'Shan', null,null, null)
